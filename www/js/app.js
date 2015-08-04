d46a25930cfe72155602e95d287e02e6879f301b
returnt <kryarek@mail.ru>
2015-08-04 15:02:42 +0300

    dell

--- www/js/app.js
+++ www/js/app.js
@@ -3,10 +3,11 @@
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-angular.module('starter', ['ionic'])
+var starter = angular.module('starter', ['ionic'])
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
+
     // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
     // for form inputs)
     if(window.cordova && window.cordova.plugins.Keyboard) {
@@ -14,6 +15,22 @@ angular.module('starter', ['ionic'])
     }
     if(window.StatusBar) {
       StatusBar.styleDefault();
+
     }
   });
-})
+});
+
+starter.controller('mainController', function($http, $scope){
+
+    $scope.submit = function(email, tel, cost){
+        $http.post('http://pot.returnt.ru/mail.php', {
+            submits: true, mail: email,
+            tel: tel, cost: cost
+            })
+            .success(function(data){
+                console.log(data);
+            }).error(function(data){
+                console.log(data);
+            });
+    };
+});
