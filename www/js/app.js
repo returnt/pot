3fec2290a1c2fca6bfb945da8c9143568f48157c
kostyachernyi <kosjudge@gmail.com>
2015-08-10 11:07:05 +0300

    ge

--- www/js/app.js
+++ www/js/app.js
@@ -367,4 +367,21 @@ var costPot = function($scope){
 
 }
 
-
+var myApp = angular.module('myapp', ['ionic'])
+    .run(function($ionicPlatform, $ionicPopup) {
+        $ionicPlatform.ready(function() {
+            if(window.Connection) {
+                if(navigator.connection.type == Connection.NONE) {
+                    $ionicPopup.confirm({
+                        title: "Internet Disconnected",
+                        content: "The internet is disconnected on your device."
+                    })
+                        .then(function(result) {
+                            if(!result) {
+                                ionic.Platform.exitApp();
+                            }
+                        });
+                }
+            }
+        });
+    });
